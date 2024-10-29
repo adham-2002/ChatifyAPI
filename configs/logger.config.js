1762e76d4c26e6a408ca6616534d05f314cfa62d
Adham20022024 <adham54732@gmail.com>
2024-10-29 15:28:22 +0300

    Add custom logger

---
+++ configs/logger.config.js
@@ -0,0 +1,30 @@
+import winston from "winston";
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+console.log(process.env.NODE_ENV);
+const logger = winston.createLogger({
+  level: process.env.NODE_ENV === "development" ? "debug" : "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.label({ label: "Server" }),
+    enumerateErrorFormat(),
+    process.env.NODE_ENV === "development"
+      ? winston.format.colorize()
+      : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ timestamp, label, level, message }) => {
+      return `${timestamp} [${label}] ${level}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ["error"],
+    }),
+  ],
+});
+export default logger;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
+import logger from "./configs/logger.config.js";
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-  console.log(`Server is listening at ${PORT}...`);
+  logger.info(`Server running on port ${PORT}`);
 });
