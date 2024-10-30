af15ff601334f323091226b13ed9df1fce91468d
Adham20022024 <adham54732@gmail.com>
2024-10-31 02:06:03 +0300

    test

--- api/index.js
+++ api/index.js
@@ -1,35 +1,20 @@
-import app from "./app.js";
-import logger from "./configs/logger.config.js";
-import dbConnection from "./configs/database.config.js";
-const PORT = process.env.PORT || 8000;
+import app from "../app.js";
+import logger from "../configs/logger.config.js";
+import dbConnection from "../configs/database.config.js";
 
 // Database connection
 dbConnection();
-// Start Server
 
-const server = app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
+export default function handler(req, res) {
+  // استخدم الدالة app لتشغيل تطبيق Express الخاص بك
+  app(req, res);
+}
+
+// معالجة الأخطاء (اختياري)
+process.on("uncaughtException", (error) => {
+  logger.error(error);
 });
 
-//! Handle Server Errors
-const exitHandler = () => {
-  if (server) {
-    logger.info("Server closed");
-    process.exit(1);
-  } else {
-    process.exit(1);
-  }
-};
-
-const unexpectedErrorHandler = (error) => {
+process.on("unhandledRejection", (error) => {
   logger.error(error);
-  exitHandler();
-};
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
-process.on("SIGTERM", () => {
-  if (server) {
-    logger.info("Server closed");
-    process.exit(1);
-  }
 });
