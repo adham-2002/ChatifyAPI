a26b140e169a20d1d136ebad563f478a5c419287
Adham20022024 <adham54732@gmail.com>
2024-10-30 04:51:44 +0300

    access and refresh tokens flow

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,29 @@
 import asyncHandler from "express-async-handler";
 import { createUser } from "../services/auth.service.js";
+import { generateToken } from "../services/token.service.js";
+
 const register = asyncHandler(async (req, res) => {
   const newUser = await createUser(req.body);
+  // generate tokens for one hour
+  const access_token = await generateToken(
+    { _id: newUser._id },
+    "1h",
+    process.env.ACCESS_TOKEN_SECRET
+  );
+  const refresh_token = await generateToken(
+    { _id: newUser._id },
+    "30d",
+    process.env.REFRESH_TOKEN_SECRET
+  );
+  res.cookie("refreshToken", refresh_token, {
+    httpOnly: true,
+    path: "/api/v1/auth/refresh-token",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
   res.status(201).json({
     status: "success",
     data: {
+      access_token,
       user: newUser,
     },
   });
